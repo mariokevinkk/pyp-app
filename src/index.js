a6fdab4e9e74c7e0cde9ff2898c6ca2e6ed4ee50
mariokevinkk <marketo2304@gmail.com>
2022-04-11 13:19:01 +0700

    second commit from pyp

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter}from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as serviceWorkerRegistration from './services/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
